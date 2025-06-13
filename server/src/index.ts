f5f3b63349950d56efddef7cd483595f6b613065
Mayke <mayke000000000@gmail.com>
2025-06-13 09:01:21 -0300

    feat: server com as rotas de user e auth + middleware de erros

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,14 @@
+import userRouter from './modules/users/user.routes'
+import authRouter from './modules/auth/auth.routes'
+import errorHandler from './middlewares/errorHandler';
 
 const express = require('express');
 const  app = express();
+
 app.use(express.json());
+app.use('/users', userRouter );
+app.use('/auth', authRouter);
 
 
+app.use(errorHandler);
 app.listen(3000, () =>console.log("Servidor rodando"));
