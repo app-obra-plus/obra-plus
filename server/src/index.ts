9cf08bb757c9362b9738d42e05db903620ca4495
Mayke <mayke000000000@gmail.com>
2025-06-19 19:21:15 -0300

    config:  configurando CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
 import userRouter from './modules/users/user.routes'
 import authRouter from './modules/auth/auth.routes'
 import errorHandler from './middlewares/errorHandler';
+import corsOptions from './config/corsConfig'
+import cors from 'cors';
 
 const express = require('express');
 const  app = express();
+app.use(cors(corsOptions));
+
 
 app.use(express.json());
 app.use('/users', userRouter );
