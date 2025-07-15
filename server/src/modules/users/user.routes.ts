61c5f2b8ceefaab6ca9f326143af545667d723bd
Mayke <mayke000000000@gmail.com>
2025-07-15 16:19:28 -0300

    doc: alterando doc

--- server/src/modules/users/user.routes.ts
+++ server/src/modules/users/user.routes.ts
@@ -25,7 +25,7 @@ const router = Router();
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/UserResponse'
+ *               $ref: '#/components/schemas/LoginResponse'
  */
 
 router.post('/', async (req: Request, res: Response) => {
@@ -52,7 +52,7 @@ router.post('/', async (req: Request, res: Response) => {
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/LoginResponse'
+ *               $ref: '#/components/schemas/UserResponse'
  *       400:
  *         description: Token mal formatado ou não fornecido
  *         content:
