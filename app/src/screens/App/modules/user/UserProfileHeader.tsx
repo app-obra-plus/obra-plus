bbf94ae6ad728e7eaba835788f04edd7003bde04
Richardy Tanure <richardy.tanure@hotmail.com>
2025-07-15 17:02:52 -0300

    feat: Mostra informações do usuário no menu

--- app/src/screens/App/modules/user/UserProfileHeader.tsx
+++ app/src/screens/App/modules/user/UserProfileHeader.tsx
@@ -1,27 +1,22 @@
-import { Button, Image, Text, View } from "react-native";
-import Feather from "@expo/vector-icons/Feather";
+import { Image, Text, View } from "react-native";
 import { useAuthStore } from "../../../../stores/useAuthStore";
 
 export function UserProfileHeader() {
   const { user, signOut } = useAuthStore();
 
+  const profilePictureUrl = user?.profile_picture || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcm5WldpI23UcsikhxKoz_ZpcyDS19ryItSw&s";
+
   return (
     <View className="flex items-center py-16">
       <View className="relative">
         <Image
-          source={{ uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcm5WldpI23UcsikhxKoz_ZpcyDS19ryItSw&s" }}
+          source={{ uri: profilePictureUrl }}
           className="w-36 h-36 rounded-full"
-        />
-        <Feather
-          className="absolute bottom-0 right-0 bg-primary color-white rounded-full p-2"
-          size={18}
-          name="edit"
-          color="white"
         />
       </View>
       <Text
         className="text-2xl font-bold mt-4"
-      >{user?.first_name}</Text>
+      >{user?.first_name} {user?.last_name}</Text>
     </View>
   );
 }
