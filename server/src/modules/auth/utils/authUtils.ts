377bdb5494250f4b0c621cf93c057c6de6aab9f8
Mayke <mayke000000000@gmail.com>
2025-09-08 19:19:45 -0300

    refactor: aumento do tempo de expiração token

--- server/src/modules/auth/utils/authUtils.ts
+++ server/src/modules/auth/utils/authUtils.ts
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken'
 
  export function  generateLoginResponse(user: any): LoginResponseDto {
         const JWT_SECRET = process.env.JWT_SECRET;
-        const EXPIRE_TOKEN = '10m'; 
+        const EXPIRE_TOKEN = '1d'; 
         if (!JWT_SECRET) {
             throw new Error('JWT_SECRET não foi definida no .env');
         }
