79a0528b2bff5f51292ae5c842432451f17f5911
Mayke <mayke000000000@gmail.com>
2025-09-12 22:52:20 -0300

    refact mudanças no dto de Advertisement + dto e mapper de image

---
+++ server/src/modules/advertisement/dto/ImageResponseDto.ts
@@ -0,0 +1,4 @@
+export interface ResponseImageDto {
+    id: string
+    url: string,
+}
--- server/src/modules/advertisement/dto/mapper/AdvertisementMapper.ts
+++ server/src/modules/advertisement/dto/mapper/AdvertisementMapper.ts
@@ -1,21 +1,24 @@
-import { Advertisement} from "../../../../generated/prisma";
+import { Advertisement } from "../../../../generated/prisma";
+import { ResponseImageDto } from "../ImageResponseDto";
 import { ResponseAdvertisementDto } from "../ResponseAdvertisementDto";
 
-
-export class AdvertisementMapper{
-    static toResponseDto(ad: Advertisement, images: string[]): ResponseAdvertisementDto{
-        const dto: ResponseAdvertisementDto = {
-            id: ad.id,
-            title: ad.title,
-            description: ad.description,
-            price: ad.price,
-            status: ad.status,
-            isDonation: ad.isDonation,
-            user_id: ad.user_id,
-            category_id: ad.category_id,
-            advertisementAddressId: ad.advertisementAddressId,
-            images
-        };
-        return(dto)
-    }
+export class AdvertisementMapper {
+  static toResponseDto(
+    ad: Advertisement,
+    images: ResponseImageDto[]
+  ): ResponseAdvertisementDto {
+    const dto: ResponseAdvertisementDto = {
+      id: ad.id,
+      title: ad.title,
+      description: ad.description,
+      price: ad.price,
+      status: ad.status,
+      isDonation: ad.isDonation,
+      user_id: ad.user_id,
+      category_id: ad.category_id,
+      advertisementAddressId: ad.advertisementAddressId,
+      images,
+    };
+    return dto;
+  }
 }
