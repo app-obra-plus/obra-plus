32ecfcd0d5400fb03ade79389ac5a1909b331112
Richardy Tanure <richardy.tanure@hotmail.com>
2025-09-22 00:14:04 -0300

    feat: Adiciona a funcionalidade de excluir um endereço

--- app/src/screens/App/modules/user/modules/Address/AddressListCard.tsx
+++ app/src/screens/App/modules/user/modules/Address/AddressListCard.tsx
@@ -1,17 +1,35 @@
 import React from "react";
-import { Image, Text, View } from "react-native";
+import { Image, Pressable, Text, View } from "react-native";
 import { AddressResponseDto } from "../../../../../../api/address/addressSch";
 import MapComponent from "../../../../../../components/MapComponent";
 import { Feather } from "@expo/vector-icons";
 import { colors } from "../../../../../../theme/colors";
+import EditDeleteDrawer from "../../../../../../components/EditDeleteDrawer";
+import { addressMdl } from "../../../../../../api/address/addressMdl";
 
 interface IAddressListCardProps {
   address: AddressResponseDto
 }
 
 export default function AddressListCard({ address }: IAddressListCardProps) {
+  const [isDrawerVisible, setIsDrawerVisible] = React.useState(false);
+
+  const handleDelete = async () => {
+    await addressMdl.delete(address.id);
+  }
+
   return (
-    <View className="bg-white rounded-xl shadow mb-4 h-fit overflow-hidden flex-row  elevation-lg">
+    <Pressable 
+      className="bg-white rounded-xl shadow mb-4 h-fit overflow-hidden flex-row  elevation-lg"
+      onPress={() => setIsDrawerVisible(true)}
+    >
+      <EditDeleteDrawer
+        isVisible={isDrawerVisible}
+        onClose={() => setIsDrawerVisible(false)}
+        onEdit={() => {}}
+        onDelete={handleDelete}
+        refreshQueryKey={["userAddresses"]}
+      />
       <View className="flex p-7 items-center justify-center">
         <Feather name="map-pin" size={32} color={colors.primary} />
       </View>
@@ -21,6 +39,6 @@ export default function AddressListCard({ address }: IAddressListCardProps) {
           {address.street}, {address.number} - {address.neighborhood}, {address.city} - {address.state}
         </Text>
       </View>
-    </View>
+    </Pressable>
   )
 }
