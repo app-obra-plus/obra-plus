4f708bf35b26e67a7aa8361a53a0912fe8ed9074
Richardy Tanure <richardy.tanure@hotmail.com>
2025-10-03 19:19:03 -0300

    fix: Corrige schema de update user

--- src/modules/users/dto/UpdateUserDto.ts
+++ src/modules/users/dto/UpdateUserDto.ts
@@ -4,7 +4,7 @@ export const UpdateUserSchema = z.object({
   first_name: z.string().min(2, 'Nome muito curto').optional(),
   last_name: z.string().min(2, 'Sobrenome muito curto').optional(),
   phone_number: z.string().min(11, 'Número inválido').optional(),
-  profile_picture: z.string().url().optional(),
+  profile_picture: z.string().url().or(z.literal("")).optional(),
 });
 
 export type UpdateUserDto = z.infer<typeof UpdateUserSchema>;
